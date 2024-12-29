821301a74d1c591b2be6d4fb50e04e9e2bc09da6
NewGen2022 <amirgimari@gmail.com>
2024-12-29 16:34:16 +0200

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,17 @@
-function App() {
-    return <></>;
-}
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Game from './pages/Game';
+
+const App = () => {
+    return (
+        <Routes>
+            {/* Home route */}
+            <Route path="/" element={<Home />} />
+
+            {/* Game routes */}
+            <Route path="/g1" element={<Game />} />
+        </Routes>
+    );
+};
 
 export default App;
