98cc9c3e59b0c631089397fddf5035425a51b2e6
NewGen2022 <amirgimari@gmail.com>
2024-12-31 14:43:26 +0200

    added responsive design

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -9,13 +9,17 @@ const Header = () => {
         <div
             className={`flex items-center place-content-between px-16 min-h-16 transition-colors ease-in-out duration-300 ${
                 theme === 'dark' ? 'bg-slate-700' : 'bg-slate-200'
-            }`}
+            } max-sm:flex-col max-sm:py-4 max-sm:gap-4`}
         >
-            <a href="/" id="back-logo" className="flex items-center">
+            <a
+                href="/"
+                id="back-logo"
+                className="flex items-center max-sm:py-2"
+            >
                 <div
                     className={`text-2xl font-bold group hover:text-cyan-400 transition-colors ease-in-out duration-300 ${
                         theme === 'dark' ? '' : 'text-slate-700'
-                    } `}
+                    } max-sm:text-4xl`}
                 >
                     Pixel
                     <span
@@ -27,19 +31,31 @@ const Header = () => {
                     </span>
                 </div>
             </a>
-            <div></div>
-            <div
-                onClick={toggleTheme}
-                className={`cursor-pointer border-2 p-2 rounded-lg transition-colors ease-in-out duration-300 ${
-                    theme === 'dark'
-                        ? 'border-yellow-300 bg-yellow-700 hover:bg-yellow-800'
-                        : 'border-slate-500 bg-slate-300 hover:bg-slate-600'
-                } `}
-            >
-                <img
-                    src={theme === 'dark' ? sunIcon : moonIcon}
-                    className="w-5"
-                />
+            <div className="flex items-center gap-5 max-sm:flex-col max-sm:gap-4">
+                <a
+                    href="/leaderboard"
+                    className={`font-semibold  transition-colors ease-in-out duration-300 ${
+                        theme === 'dark'
+                            ? 'hover:text-white'
+                            : 'text-slate-600 hover:text-black'
+                    }`}
+                >
+                    Leaderboard
+                </a>
+
+                <div
+                    onClick={toggleTheme}
+                    className={`cursor-pointer border-2 p-2 rounded-lg transition-colors ease-in-out duration-300 ${
+                        theme === 'dark'
+                            ? 'border-yellow-300 bg-yellow-700 hover:bg-yellow-800'
+                            : 'border-slate-500 bg-slate-300 hover:bg-slate-600'
+                    } `}
+                >
+                    <img
+                        src={theme === 'dark' ? sunIcon : moonIcon}
+                        className="w-5"
+                    />
+                </div>
             </div>
         </div>
     );
