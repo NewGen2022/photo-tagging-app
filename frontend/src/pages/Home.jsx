a9c368e506f4800d5f1442a819e74c50709d6ad7
NewGen2022 <amirgimari@gmail.com>
2024-12-31 14:43:50 +0200

    added styles and init template for home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,12 @@ import Layout from '../components/layout/Layout';
 const Home = () => {
     return (
         <Layout>
-            <div>HOME</div>
+            <div className="flex flex-col items-center">
+                <div className="font-bold text-4xl my-10 max-sm:my-6">
+                    Games
+                </div>
+                <div>game cards</div>
+            </div>
         </Layout>
     );
 };
