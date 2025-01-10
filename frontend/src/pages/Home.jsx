27f454a3004f78858a23f72d00796551b4d8693a
NewGen2022 <amirgimari@gmail.com>
2025-01-10 15:32:12 +0200

    added game card and page layout for scaling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,19 @@
 import Layout from '../components/layout/Layout';
+import GameCard from '../components/GameCard';
+import gameImg1 from '../assets/games/game_1.jfif';
 
 const Home = () => {
     return (
         <Layout>
-            <div className="flex flex-col items-center">
-                <div className="font-bold text-4xl my-10 max-sm:my-6">
-                    Games
+            <div className="flex flex-col items-center mx-28">
+                <h1 className="font-bold text-4xl my-10 max-sm:my-6">Games</h1>
+                <div className="flex flex-wrap justify-center gap-5 mb-10">
+                    <GameCard
+                        src={gameImg1}
+                        name={"Dragon Charmer's Island"}
+                        gameId={'1'}
+                    />
                 </div>
-                <div>game cards</div>
             </div>
         </Layout>
     );
