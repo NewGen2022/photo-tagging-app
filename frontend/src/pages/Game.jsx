4ab22861cdc039be2c17113744579ce3ac01e8a9
NewGen2022 <amirgimari@gmail.com>
2025-01-10 15:32:43 +0200

    added hook to handle game state changing

--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -1,5 +1,16 @@
+import Layout from '../components/layout/Layout';
+import useGame from '../hooks/useGame';
+import { useEffect } from 'react';
+
 const Game = () => {
-    return <div>GAME</div>;
+    const { changeIsGame } = useGame();
+
+    useEffect(() => {
+        changeIsGame(true);
+        return () => changeIsGame(false);
+    }, [changeIsGame]);
+
+    return <Layout>GAME</Layout>;
 };
 
 export default Game;
