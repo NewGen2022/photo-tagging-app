480b362834f741586d2f5fc6dbb3f39d486832c9
NewGen2022 <amirgimari@gmail.com>
2025-01-11 12:11:06 +0200

    added image and characters displaying

--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -1,16 +1,44 @@
 import Layout from '../components/layout/Layout';
 import useGame from '../hooks/useGame';
 import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import gamesData from '../gameData';
 
 const Game = () => {
     const { changeIsGame } = useGame();
+    const { gameId } = useParams();
+
+    const game = gamesData.find((game) => game.id === gameId);
 
     useEffect(() => {
-        changeIsGame(true);
-        return () => changeIsGame(false);
-    }, [changeIsGame]);
+        if (game) {
+            localStorage.setItem(
+                'gameCharacters',
+                JSON.stringify(game.characters)
+            );
+        }
 
-    return <Layout>GAME</Layout>;
+        changeIsGame(true);
+
+        return () => {
+            changeIsGame(false);
+            localStorage.removeItem('gameCharacters');
+        };
+    }, [changeIsGame, game]);
+
+    return (
+        <Layout>
+            {game ? (
+                <img
+                    src={game.image}
+                    alt={game.name}
+                    className="object-cover"
+                />
+            ) : (
+                <h1 className="text-2xl font-bold">Game not found</h1>
+            )}
+        </Layout>
+    );
 };
 
 export default Game;
