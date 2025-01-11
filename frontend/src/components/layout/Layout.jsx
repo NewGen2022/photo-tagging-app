dfbd4813540df9df4ef8d627818d5b6918786038
NewGen2022 <amirgimari@gmail.com>
2025-01-11 12:11:51 +0200

    added text centering

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
     const { theme } = useTheme();
 
     return (
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-screen text-center">
             <Header />
 
             <main
