a0ce7f129879c1132064adbe8119cf9756f610c8
NewGen2022 <amirgimari@gmail.com>
2025-01-22 20:32:20 +0200

    added getting coordinates when click on image, circle displaying when image clicked

--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -1,14 +1,28 @@
 import Layout from '../components/layout/Layout';
 import useGame from '../hooks/useGame';
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import gamesData from '../gameData';
+import { handleImageCoords } from '../js/handleImage';
+import Circle from '../components/Circle';
+import CharactersDisplay from '../components/CharactersDisplay';
 
 const Game = () => {
-    const { changeIsGame, setGameCharacters } = useGame();
+    const { changeIsGame, gameCharacters, setGameCharacters } = useGame();
     const { gameId } = useParams();
+    const [circleVisible, setCircleVisible] = useState(false);
+    const [coordinates, setCoordinates] = useState({ x: 0, y: 0 }); // normalized coordinates
+    const imgRef = useRef(null);
 
     const game = gamesData.find((game) => game.id === gameId);
+
+    const handleImgClick = (e) => {
+        setCircleVisible(true);
+        if (imgRef.current) {
+            const { x, y } = handleImageCoords(e, imgRef);
+            setCoordinates({ x, y });
+        }
+    };
 
     useEffect(() => {
         if (game) {
@@ -26,11 +40,26 @@ const Game = () => {
     return (
         <Layout>
             {game ? (
-                <img
-                    src={game.image}
-                    alt={game.name}
-                    className="object-cover"
-                />
+                <div>
+                    <img
+                        ref={imgRef}
+                        src={game.image}
+                        alt={game.name}
+                        className="object-cover"
+                        onClick={handleImgClick}
+                    />
+
+                    {circleVisible && (
+                        <>
+                            <Circle coordinates={coordinates} imgRef={imgRef} />
+                            <CharactersDisplay
+                                characters={gameCharacters}
+                                coordinates={coordinates}
+                                imgRef={imgRef}
+                            />
+                        </>
+                    )}
+                </div>
             ) : (
                 <h1 className="text-2xl font-bold">Game not found</h1>
             )}
