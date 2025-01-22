18059636c7b9444038f8a5b030ba9fd8e5818614
NewGen2022 <amirgimari@gmail.com>
2025-01-22 20:26:20 +0200

    data for game №1

---
+++ frontend/src/gameData.js
@@ -0,0 +1,29 @@
+const gamesData = [
+    {
+        id: '1098-7654-32',
+        name: "Dragon Charmer's Island",
+        image: '/games/game_1/game_1.webp',
+        characters: [
+            {
+                name: 'Raft Man',
+                image: '/games/game_1/raft-man.png',
+                found: false,
+                marker: { x: 141, y: 1735 },
+            },
+            {
+                name: 'Wizard',
+                image: '/games/game_1/wizard.png',
+                found: false,
+                marker: { x: 2195, y: 2701 },
+            },
+            {
+                name: 'Dragon',
+                image: '/games/game_1/dragon.png',
+                found: false,
+                marker: { x: 1923, y: 1742 },
+            },
+        ],
+    },
+];
+
+export default gamesData;
