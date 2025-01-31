38d7045f6bf760a3652cce805c5bda07f9ec3663
NewGen2022 <amirgimari@gmail.com>
2025-01-31 21:52:51 +0200

    added white theme

--- frontend/src/components/popup/NotFound.jsx
+++ frontend/src/components/popup/NotFound.jsx
@@ -1,12 +1,21 @@
 import notFoundIcon from '../../assets/icons/notfound.png';
+import useTheme from '../../hooks/useTheme';
 
 const NotFound = () => {
+    const { theme } = useTheme();
+
     return (
         <div
             className="flex flex-col gap-3 items-center justify-center"
             style={{ height: 'calc(100vh - 200px)' }}
         >
-            <h1 className="text-2xl font-bold">Game Not Found</h1>
+            <h1
+                className={`text-2xl font-bold ${
+                    theme === 'dark' ? '' : 'text-slate-600'
+                }`}
+            >
+                Game Not Found
+            </h1>
             <img className="w-24" src={notFoundIcon} />
         </div>
     );
