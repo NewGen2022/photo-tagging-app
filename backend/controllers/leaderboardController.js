90ca4262ff5c997ce7d82b416fbb569f2620d942
NewGen2022 <amirgimari@gmail.com>
2025-02-02 11:12:57 +0200

    changed controller to add user time

--- backend/controllers/leaderboardController.js
+++ backend/controllers/leaderboardController.js
@@ -8,8 +8,19 @@ const getUsersTime = async (req, res) => {
 const postUserTime = async (req, res) => {
     const { username, time, gameId } = req.body;
 
-    await postUserTimeDB(gameId, username, time);
-    return res.status(200).json({ message: 'Time successfully saved!' });
+    const trimmedUsername = username.trim(); // remove all trailing spaces
+    const numericTime = parseFloat(time); // Convert string back to number
+
+    try {
+        // successfully saved user time
+        await postUserTimeDB(trimmedUsername, numericTime, gameId);
+        return res.status(200).json({ message: 'Time successfully saved!' });
+    } catch (err) {
+        // error occurred saving user time
+        return res
+            .status(500)
+            .json({ error: err.message || 'Internal Server Error' });
+    }
 };
 
 module.exports = { getUsersTime, postUserTime };
